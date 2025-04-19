283a17601e7c0c60c3409e8bf1a5c3eaec6b3f4c
GabrielFeliciano <gfp001@outlook.com>
2025-04-19 20:54:36 -0300

    move map setup to init and init mini.files

--- init.lua
+++ init.lua
@@ -1,7 +1,21 @@
 require("config.lazy")
 
+
+
+-- some init configs
+vim.keymap.set({"n", "i"}, "<C-h>", function() print("hello") end)
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- default config netrw
 require("netrw").setup({})
 
 -- "tree navigation"
 require('mini.files').setup()
+
+vim.keymap.set({"n", "i"}, "<leader>gg", ":LazyGit<CR>")
+
+
