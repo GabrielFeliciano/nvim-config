d8204a7fb82063c4eb72d999b9311de85037feb6
GabrielFeliciano <gfp001@outlook.com>
2025-04-22 22:36:29 -0300

    add some things from kickstarter - needs further review tho

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 require("config.lazy")
 
-
-
 -- some init configs
 vim.keymap.set({"n", "i"}, "<C-h>", function() print("hello") end)
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -9,6 +7,9 @@ vim.keymap.set({"n", "i"}, "<C-h>", function() print("hello") end)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- default config netrw
 require("netrw").setup({})
